21b75c34fd9549f4ea5b8141500d0f50eb3ef506
illupovero <davideluporini1996@gmail.com>
2022-01-30 17:16:01 +0100

    push pre elaborazione dati

--- js/script.js
+++ js/script.js
@@ -1,115 +1,111 @@
-// const province = {
-//     'AG': 'Agrigento',
-//     'AL': 'Alessandria',
-//     'AN': 'Ancona',
-//     'AO': 'Aosta',
-//     'AR': 'Arezzo',
-//     'AP': 'Ascoli Piceno',
-//     'AT': 'Asti',
-//     'AV': 'Avellino',
-//     'BA': 'Bari',
-//     'BT': 'Barletta-Andria-Trani',
-//     'BL': 'Belluno',
-//     'BN': 'Benevento',
-//     'BG': 'Bergamo',
-//     'BI': 'Biella',
-//     'BO': 'Bologna',
-//     'BZ': 'Bolzano',
-//     'BS': 'Brescia',
-//     'BR': 'Brindisi',
-//     'CA': 'Cagliari',
-//     'CL': 'Caltanissetta',
-//     'CB': 'Campobasso',
-//     'CI': 'Carbonia-Iglesias',
-//     'CE': 'Caserta',
-//     'CT': 'Catania',
-//     'CZ': 'Catanzaro',
-//     'CH': 'Chieti',
-//     'CO': 'Como',
-//     'CS': 'Cosenza',
-//     'CR': 'Cremona',
-//     'KR': 'Crotone',
-//     'CN': 'Cuneo',
-//     'EN': 'Enna',
-//     'FM': 'Fermo',
-//     'FE': 'Ferrara',
-//     'FI': 'Firenze',
-//     'FG': 'Foggia',
-//     'FC': 'Forlì-Cesena',
-//     'FR': 'Frosinone',
-//     'GE': 'Genova',
-//     'GO': 'Gorizia',
-//     'GR': 'Grosseto',
-//     'IM': 'Imperia',
-//     'IS': 'Isernia',
-//     'SP': 'La Spezia',
-//     'AQ': 'L\'Aquila',
-//     'LT': 'Latina',
-//     'LE': 'Lecce',
-//     'LC': 'Lecco',
-//     'LI': 'Livorno',
-//     'LO': 'Lodi',
-//     'LU': 'Lucca',
-//     'MC': 'Macerata',
-//     'MN': 'Mantova',
-//     'MS': 'Massa-Carrara',
-//     'MT': 'Matera',
-//     'ME': 'Messina',
-//     'MI': 'Milano',
-//     'MO': 'Modena',
-//     'MB': 'Monza e della Brianza',
-//     'NA': 'Napoli',
-//     'NO': 'Novara',
-//     'NU': 'Nuoro',
-//     'OT': 'Olbia-Tempio',
-//     'OR': 'Oristano',
-//     'PD': 'Padova',
-//     'PA': 'Palermo',
-//     'PR': 'Parma',
-//     'PV': 'Pavia',
-//     'PG': 'Perugia',
-//     'PU': 'Pesaro e Urbino',
-//     'PE': 'Pescara',
-//     'PC': 'Piacenza',
-//     'PI': 'Pisa',
-//     'PT': 'Pistoia',
-//     'PN': 'Pordenone',
-//     'PZ': 'Potenza',
-//     'PO': 'Prato',
-//     'RG': 'Ragusa',
-//     'RA': 'Ravenna',
-//     'RC': 'Reggio Calabria',
-//     'RE': 'Reggio Emilia',
-//     'RI': 'Rieti',
-//     'RN': 'Rimini',
-//     'RM': 'Roma',
-//     'RO': 'Rovigo',
-//     'SA': 'Salerno',
-//     'VS': 'Medio Campidano',
-//     'SS': 'Sassari',
-//     'SV': 'Savona',
-//     'SI': 'Siena',
-//     'SR': 'Siracusa',
-//     'SO': 'Sondrio',
-//     'TA': 'Taranto',
-//     'TE': 'Teramo',
-//     'TR': 'Terni',
-//     'TO': 'Torino',
-//     'OG': 'Ogliastra',
-//     'TP': 'Trapani',
-//     'TN': 'Trento',
-//     'TV': 'Treviso',
-//     'TS': 'Trieste',
-//     'UD': 'Udine',
-//     'VA': 'Varese',
-//     'VE': 'Venezia',
-//     'VB': 'Verbano-Cusio-Ossola',
-//     'VC': 'Vercelli',
-//     'VR': 'Verona',
-//     'VV': 'Vibo Valentia',
-//     'VI': 'Vicenza',
-//     'VT': 'Viterbo',
-// };
+// form 1
+
+let formOne = document.getElementById('data-form');
+let formTwo = document.getElementById('data-form-2');
+let formThree = document.getElementById('data-form-3');
+let formFour = document.getElementById('data-form-4');
+let statoCivile = document.getElementById('statocivile');
+let fontedireddito = document.getElementById('fontedireddito');
+let reddito = document.getElementById('reddito-1');
+let hideElement = document.getElementById('hide-element');
+let nucleoFamiliare = document.getElementById('nucleo-familiare');
+let professione = document.getElementById('professione-2-none');
+let getSon = document.getElementById('getSon');
+let buttonNext1 = document.getElementById('button-next');
+let buttonNext2 = document.getElementById('button-next-2');
+let buttonNext3 = document.getElementById('button-next-3');
+let buttonNext4 = document.getElementById('button-next-4');
+let nFigli = document.getElementById('n-figli');
+let hideson = document.getElementById('hideson');
+let ageSon = document.getElementById('age-son');
+let ageSonDisplay = document.getElementById('age-son-display');
+let casa = document.getElementById('casa');
+let mutuo = document.getElementById('mutuo');
 
 
-// verificare questo array
+buttonNext1.addEventListener('click', function() {
+    if (statoCivile.value !== 'coniugato' && statoCivile.value !== 'coniugato' && statoCivile.value !== "vedovo") {
+        formOne.classList.add('d-none');
+        formTwo.classList.remove('d-none');
+    } else {
+        formOne.classList.add('d-none');
+        formTwo.classList.add('d-none')
+        formThree.classList.remove('d-none');
+    }
+});
+
+buttonNext2.addEventListener('click', function() {
+
+    formTwo.classList.add('d-none');
+    formThree.classList.remove('d-none');
+});
+
+buttonNext3.addEventListener('click', function() {
+    formThree.classList.add('d-none');
+    formFour.classList.remove('d-none');
+});
+
+// nascondere una domanda in base a una determinata risposta
+
+// ! necessario connettere vue per questa cosa e inserire valori dinamici negli oggetti
+reddito.addEventListener('change', function() {
+    if (reddito.value == "disoccupato") {
+        hideElement.classList.add('d-none');
+    } else {
+        hideElement.classList.remove('d-none');
+    }
+})
+
+
+nucleoFamiliare.addEventListener('change', function() {
+    if (reddito.value == "disoccupato") {
+        hideElement.classList.add('d-none');
+    } else {
+        hideElement.classList.remove('d-none');
+    }
+})
+
+getSon.addEventListener('change', function() {
+    if (getSon.value == "si") {
+        hideson.classList.remove('d-none');
+    } else {
+        hideson.classList.add('d-none');
+    }
+})
+hideson.addEventListener('change', function() {
+
+    if (getSon.value !== "null") {
+        ageSon.classList.remove('d-none');
+    } else {
+        ageSon.classList.add('d-none');
+    }
+});
+
+
+
+function generateSonAge() {
+
+    for (i = 0; i < parseInt(nFigli.value); i++) {
+
+        ageSonDisplay.innerHTML += `
+        <label class="mt-4"> età figlio ${ i +1}</label>
+        <input class="col-sm-12" type="number" value="figlio${ i +1}">
+        `;
+    }
+
+
+}
+
+
+hideson.addEventListener('change', generateSonAge)
+    // nasconere la professione nel caso in cui risponda disoccupato
+
+
+casa.addEventListener('change', function() {
+
+    if (casa.value == "proprietà") {
+        mutuo.classList.remove('d-none');
+    } else {
+        mutuo.classList.add('d-none');
+    }
+});
